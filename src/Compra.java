4d05dd4b01111bdd4af2c4c658af80c8d509bf4b
thatimoreira <thatimoreira.dev@gmail.com>
2024-07-10 03:42:15 -0300

    Implementa classe Compra com seus atributos, getters e sobrescreve método toString para exibição das compras efetuadas.

--- src/Compra.java
+++ src/Compra.java
@@ -1,4 +1,25 @@
 
-public class Compra<T> {
+public class Compra {
+    private String descricao;
+    private double valor;
+    
+    public Compra(String descricao, double valor) {
+        this.descricao = descricao;
+        this.valor = valor;
+    }
 
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+    @Override
+    public String toString() {
+        return "Compra: " + descricao + "........... R$" + valor;
+    }
+
+    
 }
